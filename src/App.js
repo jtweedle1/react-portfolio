1ec07d5cd2671cbb17403976df06457398e973f3
jtweedle1 <jazmin.tweedle@gmail.com>
2023-03-06 18:44:12 -0600

    making sure changes updated from laptop to desktop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import React, { useState } from "react";
+import Footer from './components/footer'
+import Header from './components/header'
+import Navigation from './components/navigation'
+import About from './components/about'
+import Portfolio from './components/portfolio'
+import Resume from './components/resume'
+import Contact from './components/contact'
 import './App.css';
 
+
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div></div>
+    <div>
+      <Header></Header>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer></Footer>
+    </div>
   );
 }
 
